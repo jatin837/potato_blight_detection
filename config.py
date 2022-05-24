b43d9981fdae86437295ffffc0962a73e78bd912
Jatin Pandey <pandeyjatin47@gmail.com>
2022-05-24 17:44:47 +0530

    add config variables and train code template

---
+++ config.py
@@ -0,0 +1,4 @@
+"""configs stored for model building"""
+
+IMAGE_SIZE: tuple[int, int] = (256, 256)
+BATCH_SIZE: int = 32
---
+++ train.py
@@ -0,0 +1,7 @@
+import config
+import tensorflow as tf
+from tensorflow import models, layers
+import matplotlib.pyplot as plt
+
+import tensorflow.keras.preprocessing.image_data_from_directory as load_data
+
