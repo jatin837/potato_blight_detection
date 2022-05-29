e5fd00b96a922de31af380cdf4f85c0d222868d8
Jatin Pandey <pandeyjatin47@gmail.com>
2022-05-29 12:13:45 +0530

    add model config file and docker command to run container

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ mkdir data
 cd data
 kaggle datasets download -d arjuntejaswi/plant-village
 
-# Download the TensorFlow Serving Docker image and repo
-docker pull tensorflow/serving
+
+docker run -it -p 8050:8050 -v ~/dev/potato_blight_detection:/app tensorflow/serving --rest_api_port=8050 --model_config_file=/app/model.config
