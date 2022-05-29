b70eaa784bd1aa56877cd228c8d99582cce2b6f4
Jatin Pandey <pandeyjatin47@gmail.com>
2022-05-29 11:51:16 +0530

    use fastapi and saved model to predict over REST api

---
+++ api/app.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+import requests
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# endpoint = "http://localhost:8085/v1/models/potatoes_model:predict"
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+MODEL = tf.keras.models.load_model('../models/1')
+
+@app.get("/ping")
+async def ping():
+    return "Working!!!"
+
+def read_file_as_image(data: bytes) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile
+):
+    print(f'got {file.filename}')
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+
+##  json_data = {
+##      "instances": img_batch.tolist()
+##  }
+
+#    response = requests.post(endpoint, json=json_data)
+    prediction = MODEL.predict(img_batch)
+
+    predicted_class = CLASS_NAMES[np.argmax(prediction)]
+    confidence = np.max(prediction)
+    print(predicted_class)
+
+    return {
+        "class": predicted_class,
+        "confidence": float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
